560b3af33986963847a079296189dc551bf3a6a0
phreemason <mr.omarmason@gmail.com>
2024-06-18 19:31:10 -0400

    create setup code and files for adding supabase

---
+++ lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+    getItem: (key: string) => {
+        return SecureStore.getItemAsync(key);
+    },
+    setItem: (key: string, value: string) => {
+        SecureStore.setItemAsync(key, value);
+    },
+    removeItem: (key: string) => {
+        SecureStore.deleteItemAsync(key);
+    },
+};
+
+const supabaseUrl = 'https://qwarlukbfuexhqaslwtr.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3YXJsdWtiZnVleGhxYXNsd3RyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg3NTMwMTksImV4cCI6MjAzNDMyOTAxOX0.P9g7nN7bb-n-Au9IzmBLRe5GWRcAy7AESNrfUSn3eac';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        storage: ExpoSecureStoreAdapter as any,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
+});
